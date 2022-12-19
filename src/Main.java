208d864e7c3564fd4f0546b3400af00146634212
muhammedlyas <muhammedilys@gmail.com>
2022-12-19 20:47:03 +0300

    The basics of solar system are ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem XX = new SolarSystem();
+
+        XX.sun=1;
+        XX.moon=1;
+        XX.Planet=1;
+    }
+}
